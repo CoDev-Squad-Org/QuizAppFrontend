110d1765163994c8fd486850ac142fcba3b86088
Evelyn De la cruz <evelyndc_10@hotamil.com>
2023-03-08 14:12:58 -0500

    feat: Add routes and update error messages in register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Register } from "./pages/registerPage/Register";
+import LoginForm from "./components/Login/LoginForm";
 
 function App() {
-
-	return <div className="App">Hello world</div>;;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/registro" element={<Register />} />
+        <Route path="/login" element={<LoginForm />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
