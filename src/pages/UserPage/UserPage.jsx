93c91a4a8b6367bbe6fe11f8125b380abe2f71c4
Evelyn De la cruz <evelyndc_10@hotamil.com>
2023-03-15 22:07:17 -0500

    feat: Add UserPage component

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import "./UserPage.scss";
+import { Header } from "../../components/Header/Header";
+import { Nav } from "../../components/Nav/Nav";
+import { ReturnNav } from "../../components/returnNav/ReturnNav";
+import { SearchInput } from "../../components/SearchInput/SearchInput";
+import { Carrusel } from "../../components/Carrusel/Carrusel";
 const UserPage = () => {
   return (
-    <div>UserPage</div>
+    <div className="home">
+      <Header />
+      <ReturnNav text="Descubre" />
+      <SearchInput />
+      <section className="explorerSection">
+        <span className="explorerSection__text">Mejor votados</span>
+        <Carrusel />
+        <span className="explorerSection__text">Más buscados</span>
+        <Carrusel />
+      </section>
+      <Nav />
+    </div>
   )
 }
 
