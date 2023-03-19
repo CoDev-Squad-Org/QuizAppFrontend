66f095b41fe653a014aa2cdd887517fbf62ddebf
Evelyn De la cruz <evelyndc_10@hotamil.com>
2023-03-19 01:09:12 -0500

    feat: Add SearchPage component

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Card } from '../../components/Card/Card';
+import { Header } from '../../components/Header/Header';
+import { Nav } from '../../components/Nav/Nav';
+import { ReturnNav } from '../../components/returnNav/ReturnNav';
+import { SearchInput } from '../../components/SearchInput/SearchInput';
+import "./SearchPage.scss";
 
 const SearchPage = () => {
   return (
-    <div>SearchPage</div>
+    <div className='searchPage'>
+        <Header color="blue"/>
+        <ReturnNav text="Busqueda"/>
+        <SearchInput/>
+        <section className='searchPageSection'>
+            <Card lenguage="CSS" level="Intermedio"/>
+            <Card lenguage="HTML" level="Fácil"/>
+        </section>
+        <Nav/>
+    </div>
   )
 }
 
